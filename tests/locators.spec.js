0783a0004707b9fd52ab7d00ba90e08447652383
Rafael Mejia <rafaeldj4@gmail.com>
2025-03-31 22:02:52 -0400

    Adding Locators tests

--- tests/locators.spec.js
+++ tests/locators.spec.js
@@ -4,6 +4,31 @@ test("Playwright Locators", async ({page})=>{
 
     await page.goto("https://rahulshettyacademy.com/loginpagePractise")
     console.log(await page.title())
-    console.log("Testing")
+    
+    await page.locator("input#username").fill("rahulshettyacademy ");
+    await page.locator("input[name='password']").fill("learning");
+    await page.locator("input#terms").click();
+    await page.locator("#signInBtn").click();
+
+});
+
+test("Successful Login", async ({page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+    await page.locator("#username").fill("rahulshettyacademy");
+    await page.locator("#password").fill("learning");
+    await page.locator("#signInBtn").click();
+    await expect(page).toHaveTitle("ProtoCommerce");
+    console.log(await page.title())
+
+});
+
+test("Failed Login", async ({page})=>{
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+    await page.locator("#username").fill("rahulshetty");
+    await page.locator("#password").fill("learning");
+    await page.locator("#signInBtn").click();
+    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.")
 
 });
